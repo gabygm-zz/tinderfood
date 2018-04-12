24d18ef9866fb450b317c34ba4ad6929303aba14
Gaby G <paogaby0803@gmail.com>
2018-04-12 08:15:23 -0500

    dish food like

--- apps/food/admin.py
+++ apps/food/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from apps.food.models import Client, DishFood, ClientListFood, ListItem
+
+admin.site.register(Client)
+admin.site.register(DishFood)
+admin.site.register(ClientListFood)
+admin.site.register(ListItem)
--- apps/food/views.py
+++ apps/food/views.py
@@ -1,3 +1,28 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from apps.food.models import DishFood, ClientListFood, Client, ListItem
+
+
+class FishFoodLikeView(APIView):
+    permission_classes = (IsAuthenticated,)
+    renderer_classes = (JSONRenderer,)
+
+    def put(self, request, fish_id):
+        try:
+            dish_food = DishFood.objects.get(id=fish_id)
+            client = Client.objects.get(user=self.request.user)
+        except ObjectDoesNotExist:
+            return Response({"error": "No DishFood with that ID found"}, status=status.HTTP_404_NOT_FOUND)
+        if dish_food.likes > 0:
+            dish_food.likes += 1
+            dish_food.save()
+        obj, created = ClientListFood.objects.get_or_create(name="favorite_dishes", client=client)
+        item, created = ListItem.objects.get_or_create(list=obj, item=fish_id)
+
+        return Response('liked', status=status.HTTP_200_OK)
